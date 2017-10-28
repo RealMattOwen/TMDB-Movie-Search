423b9ee82ee0b55da6f8fe2d35607ca6a211c6c4
Matthew Owen <matty101@live.co.uk>
2017-10-28 23:54:11 +0100

    Updated background styling

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -8,7 +8,7 @@ export class MovieDetails extends React.Component {
                 <div className="content-container">
                     <div className="movie-details">
                         <div className="section">
-                            <img src={`http://image.tmdb.org/t/p/original/${this.props.movie.coverImage}?api_key=375d33fe34920fdf8cb02b7ce3600ba7`} />
+                            <img src={`https://image.tmdb.org/t/p/original/${this.props.movie.coverImage}?api_key=375d33fe34920fdf8cb02b7ce3600ba7`} />
                         </div>
                         <div className="section">
                             <h1 className="movie-title">{this.props.movie.title}</h1>
