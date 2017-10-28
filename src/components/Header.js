80a54e24f2531149925b41fd77defea637f01d3d
Matthew Owen <matty101@live.co.uk>
2017-10-28 22:16:49 +0100

    Current state of the app

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,51 +28,51 @@ export class Header extends React.Component {
         }
     };
     movieDetailsRequest = e => {
-        console.log(e);
+        // console.log(e);
         if (e.key === 'Enter') {
-            console.log('--------------------------');
-            console.log(e);
-            // let movieId;
-            // const options = Array.from(document.getElementsByTagName('option'));
-            // for (let i = 0; i < options.length; i++) {
-            //     if (options[i].value === e.target.value) {
-            //         movieId = options[i].id;
-            //     }
-            // }
-            //
-            // axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=375d33fe34920fdf8cb02b7ce3600ba7`)
-            //     .then(res => {
-            //         const {
-            //             id,
-            //             backdrop_path: backgroundImage,
-            //             poster_path: coverImage,
-            //             title,
-            //             tagline: tagLine,
-            //             overview,
-            //             release_date: releaseDate,
-            //             runtime,
-            //             revenue: boxOffice,
-            //             vote_average: voteAverage
-            //         } = res.data;
-            //         const movie = {
-            //             id,
-            //             backgroundImage,
-            //             coverImage,
-            //             title,
-            //             tagLine,
-            //             overview,
-            //             releaseDate,
-            //             runtime,
-            //             boxOffice,
-            //             voteAverage
-            //         };
-            //         this.props.update({
-            //             movie
-            //         });
-            //     })
-            //     .catch(error => {
-            //         console.log(error);
-            //     });
+            // console.log('--------------------------');
+            // console.log(e);
+            let movieId;
+            const options = Array.from(document.getElementsByTagName('option'));
+            for (let i = 0; i < options.length; i++) {
+                if (options[i].value === e.target.value) {
+                    movieId = options[i].id;
+                }
+            }
+
+            axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=375d33fe34920fdf8cb02b7ce3600ba7`)
+                .then(res => {
+                    const {
+                        id,
+                        backdrop_path: backgroundImage,
+                        poster_path: coverImage,
+                        title,
+                        tagline: tagLine,
+                        overview,
+                        release_date: releaseDate,
+                        runtime,
+                        revenue: boxOffice,
+                        vote_average: voteAverage
+                    } = res.data;
+                    const movie = {
+                        id,
+                        backgroundImage,
+                        coverImage,
+                        title,
+                        tagLine,
+                        overview,
+                        releaseDate,
+                        runtime,
+                        boxOffice,
+                        voteAverage
+                    };
+                    this.props.update({
+                        movie
+                    });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     };
     render() {
@@ -84,8 +84,8 @@ export class Header extends React.Component {
                             <img src="https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg" />
                         </a>
                         <div className="search">
-                            <input list="movies" type="text" placeholder="Search Movie Title" className="text-input" onChange={e => this.movieNameRequest(e)} />
-                            <datalist id="movies"  onKeyPress={e => this.movieDetailsRequest(e)}>
+                            <input list="movies" type="text" placeholder="Search Movie Title" className="text-input" onChange={e => this.movieNameRequest(e)} onKeyPress={e => this.movieDetailsRequest(e)} />
+                            <datalist id="movies">
                                 {this.state.movies ? this.state.movies.map(movie => {
                                     return <option key={movie.id} id={movie.id} value={movie.title} />
                                 }) : ''}
